6e2d42c6c394e35c06ee73f4e750c0ffb35273d7
Rununu Games <ngugikerei@gmail.com>
2023-08-31 12:24:30 +0300

    Create Node Server and Install Nodemon

---
+++ backend/data.js
@@ -0,0 +1,100 @@
+const data = {
+  animals: [
+    {
+      name: 'Jersey Cattle',
+      slug: 'jersey-cat',
+      breed: 'Jersey',
+      age: '5',
+      image: '/images/jersey.jpg',
+      VaccinationDetails: '',
+      ListPrice: 200000,
+      County: 'Kisii',
+      OpenToOffers: 'Yes',
+    },
+
+    {
+      name: 'Angus Cattle',
+      slug: 'angus-cat',
+      breed: 'Angus',
+      age: '5',
+      image: '/images/angus.jpg',
+      VaccinationDetails: '',
+      ListPrice: 300000,
+      County: 'Kiambu',
+      OpenToOffers: 'Yes',
+    },
+
+    {
+      name: 'Holsteins Fresian',
+      slug: 'holsteins-cat',
+      breed: 'Holsteins',
+      age: '7',
+      image: '/images/holsteins.jpg',
+      VaccinationDetails: '',
+      ListPrice: 250000,
+      County: 'Nairobi',
+      OpenToOffers: 'No',
+    },
+
+    {
+      name: 'Sussex Layer',
+      slug: 'sussex-chick',
+      breed: 'Sussex Chicken',
+      age: '1',
+      image: '/images/sussex.jpg',
+      VaccinationDetails: '',
+      ListPrice: 20000,
+      County: 'Kirinyaga',
+      OpenToOffers: 'Yes',
+    },
+
+    {
+      name: 'Sussex Layer',
+      slug: 'sussex-chick',
+      breed: 'Sussex Chicken',
+      age: '1',
+      image: '/images/sussex.jpg',
+      VaccinationDetails: '',
+      ListPrice: 20000,
+      County: 'Kirinyaga',
+      OpenToOffers: 'Yes',
+    },
+
+    {
+      name: 'Angus Cattle',
+      slug: 'angus-cat',
+      breed: 'Angus',
+      age: '5',
+      image: '/images/angus.jpg',
+      VaccinationDetails: '',
+      ListPrice: 300000,
+      County: 'Kiambu',
+      OpenToOffers: 'Yes',
+    },
+
+    {
+      name: 'Jersey Cattle',
+      slug: 'jersey-cat',
+      breed: 'Jersey',
+      age: '5',
+      image: '/images/jersey.jpg',
+      VaccinationDetails: '',
+      ListPrice: 200000,
+      County: 'Kisii',
+      OpenToOffers: 'Yes',
+    },
+    {
+      name: 'Holsteins Fresian',
+      slug: 'holsteins-cat',
+      breed: 'Holsteins',
+      age: '7',
+      image: '/images/holsteins.jpg',
+      VaccinationDetails: '',
+      ListPrice: 250000,
+      County: 'Nairobi',
+      OpenToOffers: 'No',
+    },
+  ],
+};
+
+export default data;
