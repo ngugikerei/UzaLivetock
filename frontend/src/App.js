abee004fe91c9571230f94a6c9aae86c2da80499
Rununu Games <ngugikerei@gmail.com>
2023-09-05 11:43:40 +0300

    Add Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,38 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">UZALIVESTOCK</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Uza Livestock</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
 
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:slug" element={<ProductScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+
+        <footer>
+          <div className="text-center">
+            UzaLivestock. All Rights Reserved 2023
+          </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
